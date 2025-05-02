49b0a8be6261dd322ba4c7f5e238a29fee5c6742
Bhanu Bhashkar <reachoutbhanu@gmail.com>
2025-05-02 23:06:56 +0530

    changes

--- 00_Notes.md
+++ 00_Notes.md
@@ -108,6 +108,49 @@ app.listen(port, () => {
 
 ```
 
+### Access File Path Dynamically
+```js
+import express from "express";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res)=>{
+    //console.log(__dirname + "/public/form.html");
+    res.sendFile(__dirname + "/public/form.html");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+
+```
+
+### Get form data from POST event
+```js
+import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended : true }));
+
+app.post("/form", (req,res)=>{
+    console.log(req.body);
+    const name = req.body.fname + req.body.lname
+    res.send('Your username is : ' + name)
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+
+```
+
 ### Create HTTP Request
 ```js
 
--- 03_Middleware/project.js
+++ 03_Middleware/project.js
@@ -7,15 +7,42 @@ const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+var userActive = false;
+
 app.use(bodyParser.urlencoded({extended:true}));
+
+// First way to create a middleware and validate user
+function passwordCheck(req,res,next){
+    if(req.body.cred==="Passw0rd"){
+        userActive = true;
+    }else{
+        res.sendFile(__dirname+"/public/index.html")
+    }
+    next()
+}
+
+app.use(passwordCheck);
 
 app.get("/", (req,res) =>{
     res.send('Hi');
     //res.sendFile(__dirname+"public/project.html")
 })
 
-app.post("/validate", (req,res)=>{
-    if(req.body.cred==="Passw0rd"){
+// First way to create a middleware and validate user
+app.post("/validateOne", (req,res)=>{
+    if(userActive){
+        userActive = false;
+        res.sendFile(__dirname+"/public/project.html")
+    }else{
+        res.sendFile(__dirname+"/public/index.html")
+    }
+})
+
+// Second way to validate password in method itself
+app.post("/validateTwo", (req,res)=>{
+    
+    //if(req.body.cred==="Passw0rd"){
+    if(req.body["cred"]==="Passw0rd"){
         res.sendFile(__dirname+"/public/project.html")
     }else{
         res.sendFile(__dirname+"/public/index.html")
